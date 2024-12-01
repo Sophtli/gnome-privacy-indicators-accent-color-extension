83e16695df55cd8d798461398622ea4ebac34689
Sophtli <mail@sopht.li>
2024-12-02 00:36:05 +0100

    create constant keys

--- extension.js
+++ extension.js
@@ -1,6 +1,17 @@
 import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 import * as Main from "resource:///org/gnome/shell/ui/main.js";
 
+const PRIVACY_INDICATORS = "privacy-indicators";
+const SCREEN_SHARING_INDICATOR = "screen-sharing-indicator";
+const SCREEN_RECORDING_INDICATOR = "screen-recording-indicator";
+const BLUR = "blur";
+
+const PRIVACY_INDICATORS_CLASS = "privacy-indicators-accent-color";
+const SCREEN_SHARING_INDICATOR_CLASS = "screen-sharing-indicator-accent-color";
+const SCREEN_RECORDING_INDICATOR_CLASS =
+  "screen-recording-indicator-accent-color";
+const BLUR_CLASS = "screen-sharing-recording-indicators-blur";
+
 export default class PrivacyIndicatorsAccentColor extends Extension {
   enable() {
     this._settings = this.getSettings();
@@ -10,70 +21,98 @@ export default class PrivacyIndicatorsAccentColor extends Extension {
     this._onScreenRecordingIndicatorChanged();
     this._onBlurChanged();
 
-    this._settings.connect(
-      "changed::privacy-indicators",
+    this._onPrivacyIndicatorsChangedID = this._settings.connect(
+      "changed::" + PRIVACY_INDICATORS,
       this._onPrivacyIndicatorsChanged.bind(this),
     );
-    this._settings.connect(
-      "changed::screen-sharing-indicator",
+    this._onScreenSharingIndicatorChangedID = this._settings.connect(
+      "changed::" + SCREEN_SHARING_INDICATOR,
       this._onScreenSharingIndicatorChanged.bind(this),
     );
-    this._settings.connect(
-      "changed::screen-recording-indicator",
+    this._onScreenRecordingIndicatorChangedID = this._settings.connect(
+      "changed::" + SCREEN_RECORDING_INDICATOR,
       this._onScreenRecordingIndicatorChanged.bind(this),
     );
-    this._settings.connect("changed::blur", this._onBlurChanged.bind(this));
+    this._onBlurChangedID = this._settings.connect(
+      "changed::" + BLUR,
+      this._onBlurChanged.bind(this),
+    );
   }
 
   disable() {
     this._settings = null;
+
+    if (this._onPrivacyIndicatorsChangedID) {
+      global.settings.disconnect(this._onPrivacyIndicatorsChangedID);
+      this._onPrivacyIndicatorsChangedID = null;
+    }
+
+    if (this._onScreenSharingIndicatorChangedID) {
+      global.settings.disconnect(this._onScreenSharingIndicatorChangedID);
+      this._onScreenSharingIndicatorChangedID = null;
+    }
+
+    if (this._onScreenRecordingIndicatorChangedID) {
+      global.settings.disconnect(this._onScreenRecordingIndicatorChangedID);
+      this._onScreenRecordingIndicatorChangedID = null;
+    }
+
+    if (this._onBlurChangedID) {
+      global.settings.disconnect(this._onBlurChangedID);
+      this._onBlurChangedID = null;
+    }
+
+    Main.layoutManager.uiGroup.remove_style_class_name(
+      PRIVACY_INDICATORS_CLASS,
+    );
+    Main.layoutManager.uiGroup.remove_style_class_name(
+      SCREEN_SHARING_INDICATOR_CLASS,
+    );
+    Main.layoutManager.uiGroup.remove_style_class_name(
+      SCREEN_RECORDING_INDICATOR_CLASS,
+    );
+    Main.layoutManager.uiGroup.remove_style_class_name(BLUR_CLASS);
   }
 
   _onPrivacyIndicatorsChanged() {
-    if (this._settings.get_boolean("privacy-indicators")) {
-      Main.layoutManager.uiGroup.add_style_class_name(
-        "privacy-indicators-accent-color",
-      );
+    if (this._settings.get_boolean(PRIVACY_INDICATORS)) {
+      Main.layoutManager.uiGroup.add_style_class_name(PRIVACY_INDICATORS_CLASS);
       return;
     }
     Main.layoutManager.uiGroup.remove_style_class_name(
-      "privacy-indicators-accent-color",
+      PRIVACY_INDICATORS_CLASS,
     );
   }
 
   _onScreenSharingIndicatorChanged() {
-    if (this._settings.get_boolean("screen-sharing-indicator")) {
+    if (this._settings.get_boolean(SCREEN_SHARING_INDICATOR)) {
       Main.layoutManager.uiGroup.add_style_class_name(
-        "screen-sharing-indicator-accent-color",
+        SCREEN_SHARING_INDICATOR_CLASS,
       );
       return;
     }
     Main.layoutManager.uiGroup.remove_style_class_name(
-      "screen-sharing-indicator-accent-color",
+      SCREEN_SHARING_INDICATOR_CLASS,
     );
   }
 
   _onScreenRecordingIndicatorChanged() {
-    if (this._settings.get_boolean("screen-recording-indicator")) {
+    if (this._settings.get_boolean(SCREEN_RECORDING_INDICATOR)) {
       Main.layoutManager.uiGroup.add_style_class_name(
-        "screen-recording-indicator-accent-color",
+        SCREEN_SHARING_INDICATOR_CLASS,
       );
       return;
     }
     Main.layoutManager.uiGroup.remove_style_class_name(
-      "screen-recording-indicator-accent-color",
+      SCREEN_SHARING_INDICATOR_CLASS,
     );
   }
 
   _onBlurChanged() {
-    if (this._settings.get_boolean("blur")) {
-      Main.layoutManager.uiGroup.add_style_class_name(
-        "screen-sharing-recording-indicators-blur",
-      );
+    if (this._settings.get_boolean(BLUR)) {
+      Main.layoutManager.uiGroup.add_style_class_name(BLUR_CLASS);
       return;
     }
-    Main.layoutManager.uiGroup.remove_style_class_name(
-      "screen-sharing-recording-indicators-blur",
-    );
+    Main.layoutManager.uiGroup.remove_style_class_name(BLUR_CLASS);
   }
 }
