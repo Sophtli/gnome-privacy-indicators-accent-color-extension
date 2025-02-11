307a09d25dd48bcd1761b6d0a17e47999d90aa4d
Sophtli <mail@sopht.li>
2025-02-11 23:48:52 +0100

    reorganize structure, add translation (german)

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Privacy Indicators Accent Color
+
 Apply the system accent color to privacy indicators (e.g., microphone) and screen sharing/recording indicators, with an optional blurred background, in the top bar
+
+# Package
+
+```
+gnome-extensions pack src --podir=../po --extra-source="../LICENSE"
+```
+
+# Translation
+
+## Update .pot file
+
+```
+xgettext --from-code=UTF-8 --package-name="Privacy Indicators Accent Color" --copyright-holder="Sophtli" --output=po/privacy-indicators-accent-color@sopht.li.pot src/*.js
+```
+
+## Create new .po file
+
+```
+msginit --locale de
+```
+
+## Update .po file
+
+```
+msgmerge -U po/de.po po/privacy-indicators-accent-color@sopht.li.pot
+```
--- src/prefs.js
+++ src/prefs.js
@@ -1,43 +1,44 @@
 import Gio from "gi://Gio";
 import Adw from "gi://Adw";
 
-import { ExtensionPreferences } from "resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js";
+import {
+  ExtensionPreferences,
+  gettext as _,
+} from "resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js";
 
 export default class PrivacyIndicatorsAccentColorPreferences extends ExtensionPreferences {
   fillPreferencesWindow(window) {
-    const page = new Adw.PreferencesPage({
-      title: "General",
-      icon_name: "dialog-information-symbolic",
-    });
+    const page = new Adw.PreferencesPage();
     window.add(page);
 
     const group = new Adw.PreferencesGroup({
-      title: "Appearance",
+      title: _("Appearance"),
     });
     page.add(group);
 
     const privacyIndicatorsRow = new Adw.SwitchRow({
-      title: "Privacy Indicators",
-      subtitle: "Whether to recolor the privacy indicators",
+      title: _("Privacy Indicators"),
+      subtitle: _("Recolor the privacy indicators"),
     });
     group.add(privacyIndicatorsRow);
 
     const screenSharingIndicatorRow = new Adw.SwitchRow({
-      title: "Screen Sharing Indicator",
-      subtitle: "Whether to recolor the screen sharing indicator",
+      title: _("Screen Sharing Indicator"),
+      subtitle: _("Recolor the screen sharing indicator"),
     });
     group.add(screenSharingIndicatorRow);
 
     const screenRecordingIndicatorRow = new Adw.SwitchRow({
-      title: "Screen Recording Indicator",
-      subtitle: "Whether to recolor the screen recording indicator",
+      title: _("Screen Recording Indicator"),
+      subtitle: _("Recolor the screen recording indicator"),
     });
     group.add(screenRecordingIndicatorRow);
 
     const blurRow = new Adw.SwitchRow({
-      title: "Blur",
-      subtitle:
-        "Whether to apply blur to the screen sharing and recording indicators, this setting only works if the indicators are recolored",
+      title: _("Blur"),
+      subtitle: _(
+        "Apply blur to the screen sharing and recording indicators; this setting only works if the indicators are recolored"
+      ),
     });
     group.add(blurRow);
 
@@ -46,25 +47,25 @@ export default class PrivacyIndicatorsAccentColorPreferences extends ExtensionPr
       "privacy-indicators",
       privacyIndicatorsRow,
       "active",
-      Gio.SettingsBindFlags.DEFAULT,
+      Gio.SettingsBindFlags.DEFAULT
     );
     window._settings.bind(
       "screen-sharing-indicator",
       screenSharingIndicatorRow,
       "active",
-      Gio.SettingsBindFlags.DEFAULT,
+      Gio.SettingsBindFlags.DEFAULT
     );
     window._settings.bind(
       "screen-recording-indicator",
       screenRecordingIndicatorRow,
       "active",
-      Gio.SettingsBindFlags.DEFAULT,
+      Gio.SettingsBindFlags.DEFAULT
     );
     window._settings.bind(
       "blur",
       blurRow,
       "active",
-      Gio.SettingsBindFlags.DEFAULT,
+      Gio.SettingsBindFlags.DEFAULT
     );
   }
 }
